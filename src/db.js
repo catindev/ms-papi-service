94ecb89158d94cf293f3cc43450e7e3e2e45b322
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-11-22 20:53:44 +0600

    mlab migration

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { log, warn } = console
 
 mongoose.Promise = Promise
 
-mongoose.connection.openUri('mongodb://localhost/ms3')
+mongoose.connection.openUri('mongodb://ms3usr:mp7u@ds117876-a0.mlab.com:17876,ds117876-a1.mlab.com:17876/ms3?replicaSet=rs-ds117876')
 	.once('open', () => log('MongoDB connected'))
 	.on('close', () => {
 		log('MongoDB connection closed')
