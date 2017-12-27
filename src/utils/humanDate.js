d9ad0fd43ba888c6e44ed2e3131c5ffab92ed5af
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-27 09:27:35 +0600

    Task feature fix

--- src/utils/humanDate.js
+++ src/utils/humanDate.js
@@ -1,24 +1,9 @@
 const moment = require('moment')
 moment.locale('ru')
 
-function formatDateForHumans(originalDate, noTime = false) {
-    const callDate = moment(originalDate)
-    const today = moment(new Date())
-    return today.diff(callDate, 'hours') <= 23 ?
-        callDate.fromNow() :
-        callDate.format('DD MMMM HH:mm')
-}
-
-function isToday(mDate) {
-    const today = moment().startOf('day')
-    return mDate.isSame(today, 'd')
-}
-
-function formatDate(originalDate) {
+module.exports = function formatDate(originalDate, noTime = false) {
     const callDate = moment(originalDate)
     return isToday(callDate) ?
         'Сегодня, ' + callDate.format('D MMMM' + noTime? '' : '  HH:mm') :
         callDate.format('D MMMM' + noTime? '' : '  HH:mm')
 }
-
-module.exports = formatDate
