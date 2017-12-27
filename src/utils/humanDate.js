7a3f125449d286ce8c3b3dddbfabe868b5bfd856
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-27 09:30:06 +0600

    Task feature fix

--- src/utils/humanDate.js
+++ src/utils/humanDate.js
@@ -8,7 +8,8 @@ function isToday(mDate) {
 
 module.exports = function formatDate(originalDate, noTime = false) {
     const callDate = moment(originalDate)
+    const format = 'D MMMM' + noTime? '' : '  HH:mm'
     return isToday(callDate) ?
-        'Сегодня, ' + callDate.format('D MMMM' + noTime? '' : '  HH:mm') :
-        callDate.format('D MMMM' + noTime? '' : '  HH:mm')
+        'Сегодня, ' + callDate.format(format) :
+        callDate.format(format)
 }
